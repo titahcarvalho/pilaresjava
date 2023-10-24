99afa5e1187e4bf708b355a532bd2fae3027f3ab
Tita <titahcarvalho@hotmail.com>
2023-10-24 19:38:50 -0300

    Introdução à Herança - utilização do extends para a reutilização das variáveis e métodos da classe Carro.

--- Carro.java
+++ Carro.java
@@ -1,9 +1,28 @@
 package poo;
-
+import java.util.Random;
 public class Carro { //classe apenas como modelo
 	int ano;
 	String cor; //dois atributos
-	
+	public Carro() { //construtor sem parametros
+		Random gerador = new Random();
+		int chassi = gerador.nextInt(1000);
+		System.out.println("Chassi:" + chassi );
+		
+	}
+							//agora possui dois parametros nesse construtor
+	public Carro(int ano, String cor){ //todo construtor precisa ter o memso nome da classe
+		this.ano = ano;
+		this.cor = cor;
+				 
+         System.out.println("           _____________");
+         System.out.println("          /    /     |  \\");
+         System.out.println(" ________/____/______|___\\");
+         System.out.println(" [0[\\\\[0[___    |  - ____||");
+         System.out.println(" \\_____|//. \\|_____|/, \\//");
+         System.out.println("   \\_\\ \\__//        \\__//");
+         System.out.println("  _________**UNO**___________________");
+            
+	}
 	void ligar() {
 		System.out.println("Engine on...");
 	}
